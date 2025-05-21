b7e1662e7adbc0752994a4469cbfcceb51333978
XxBeatDiaz <beatdiazmusic@gmail.com>
2025-05-21 12:13:12 +0300

    Update CombatTool.cs

--- CombatTool.cs
+++ CombatTool.cs
@@ -4,19 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IDFOpertion
+namespace IDFOpertion.Models
 {
     public abstract class AttackTools
     {
         string TypeBomb;
         string EffectiveAgainst;
         int AvailableAttacks;
-
-        public AttackTools(string typeBomb, string effectiveAgainst, int availableAttacks)
-        {
-            TypeBomb = typeBomb;
-            EffectiveAgainst = effectiveAgainst;
-            AvailableAttacks = availableAttacks;
-        }
     }
 }
