c04ada1813b0d97fd82339b53b7c9b94c3d36179
BeatDiaz <beatdiazmusic@gmail.com>
2025-05-21 12:24:37 +0300

    Class Terrorists

--- Hamas.cs
+++ Hamas.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace IDFOpertion
+namespace IDFOpertion.Models
 {
     public class Hamas
     {
