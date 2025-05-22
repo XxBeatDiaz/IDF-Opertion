8426bfab973850059d720e78bd06ee1fc69bf1b6
orel <0533orel@gmail.com>
2025-05-22 16:47:46 +0300

    merge lists and delete unuseless files

---
+++ HelperLists.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFOpertion.Models
+{
+    public class TerroristList
+    {
+        public List<Terrorist> terrorists = new List<Terrorist>() 
+        {
+            new Terrorist("muhamad", 1, "alive", ["gun"]),
+            new Terrorist("ali", 2, "alive", ["gun", "knife"]),
+            new Terrorist("yusuf", 3, "alive", ["gun", "shotgun"])
+        };
+    }
+
+
+    public class CombatToolsList
+    {
+        public List<object> wapens = new List<object>
+        {
+            new F16FighterJet(),
+            new Hermes460Drone(),
+            new Artillery()
+        };
+    }
+
+    public class Places
+    {
+        public List<string> places = new List<string>
+        {
+            "han yunes",
+            "gazza",
+            "yosh",
+            "levanon"
+        };
+    }
+
+
+    public class DateTime
+    {
+        public List<double> dateTime = new List<double>
+        {
+            2024,
+            2023,
+            2002,
+            2001,
+            2009,
+            2008,
+            2007,
+            2006
+        };
+    }
+
+}
